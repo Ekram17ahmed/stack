cc864f96265fe631c27ef26d0cbfb911d4d9909c
ekram <ekram.hafez03@eng-st.cu.edu.eg>
2025-07-24 13:02:24 +1100

    Add project files.

---
+++ Stack/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the stack: ");
+            int size = int.Parse(Console.ReadLine());
+            StackClass myStack = new StackClass(size);
+
+            while (true)
+            {
+                Console.WriteLine("\nChoose an operation:");
+                Console.WriteLine("1 - Push");
+                Console.WriteLine("2 - Pop");
+                Console.WriteLine("3 - Peek");
+                Console.WriteLine("4 - IsEmpty");
+                Console.WriteLine("5 - IsFull");
+                Console.WriteLine("6 - Exit");
+                Console.Write("Your choice: ");
+
+                string input = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("Enter a number to push: ");
+                        int item = int.Parse(Console.ReadLine());
+                        myStack.Push(item);
+                        break;
+
+                    case "2":
+                        int popped = myStack.Pop();
+                        if (popped != -1)
+                            Console.WriteLine(" Popped: " + popped);
+                        break;
+
+                    case "3":
+                        int top = myStack.Peek();
+                        if (top != -1)
+                            Console.WriteLine("Top element is: " + top);
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Stack is " + (myStack.IsEmpty() ? "empty." : "not empty."));
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Stack is " + (myStack.IsFull() ? "full." : "not full."));
+                        break;
+
+                    case "6":
+                        Console.WriteLine(" Exiting the program.");
+                        return;
+
+                    default:
+                        Console.WriteLine(" Invalid choice. Try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Stack/StackClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class StackClass
+    {
+        private int[] items;
+        private int top;
+        private int maxSize;
+
+        public StackClass(int size)
+        {
+            maxSize = size;
+            items = new int[maxSize];
+            top = -1;
+        }
+
+        public void Push(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine(" Stack Overflow! Cannot push " + item);
+                return;
+            }
+            items[++top] = item;
+            Console.WriteLine(item + " pushed to stack.");
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine(" Stack Underflow! Cannot pop.");
+                return -1;
+            }
+            return items[top--];
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine(" Stack is empty.");
+                return -1;
+            }
+            return items[top];
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public bool IsFull()
+        {
+            return top == maxSize - 1;
+        }
+    }
+
+}
